22b30609d46fb9d742c00c339cf92c136bfcfdd1
Farhan Khan <farhan01617@gmail.com>
2019-02-28 15:15:09 +0600

    update

--- inc/elementor/widgets/widget-service.php
+++ inc/elementor/widgets/widget-service.php
@@ -70,7 +70,7 @@ class thegncy_Widget_Service extends Widget_Base {
       ?>
  
       <div class="service-item-2">
-         <?php echo wp_get_attachment_image($settings['icon']); ?>
+         <i class="<?php echo wp_get_attachment_image($settings['icon']); ?>"></i>
          <h5><?php echo esc_html($settings['title']); ?></h5>
          <p><?php echo esc_html($settings['text']); ?></p>
       </div>
