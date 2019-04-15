aa74a84d75cd29a9a476fbd6b3de33ed00e69923
Farhan Khan <farhan01617@gmail.com>
2019-04-15 10:40:39 +0600

    update

--- inc/elementor/widgets/widget-portfolio.php
+++ inc/elementor/widgets/widget-portfolio.php
@@ -33,56 +33,6 @@ class felipa_Widget_Portfolio extends Widget_Base {
       );
 
       $this->add_control(
-         'style',
-         [
-            'label' => __( 'Style', 'felipa' ),
-            'type' => \Elementor\Controls_Manager::SELECT,
-            'default' => 'one',
-            'options' => [
-               'one'  => __( 'One', 'felipa' ),
-               'two' => __( 'Two', 'felipa' )
-            ],
-         ]
-      );
-
-      $this->add_control(
-         'sub-title',
-         [
-            'label' => __( 'Sub title', 'felipa' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => 'Projects', 
-            'condition' => [
-               'style' => 'two',
-            ]
-         ]
-         
-      );
-
-      $this->add_control(
-         'title',
-         [
-            'label' => __( 'Title', 'felipa' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => 'Check our portfolios',
-            'condition' => [
-               'style' => 'two',
-            ]
-         ]
-      );
-
-      $this->add_control(
-         'deacription',
-         [
-            'label' => __( 'Deacription', 'felipa' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => 'Lorem ipsum dolor sit amet, consectetur sicing elit, sed do eiusmod tempor incidid ut labore et dolore',
-            'condition' => [
-               'style' => 'two',
-            ]
-         ]
-      );
-
-      $this->add_control(
          'ppp',
          [
             'label' => __( 'Number of Portfolio', 'felipa' ),
@@ -101,18 +51,6 @@ class felipa_Widget_Portfolio extends Widget_Base {
          ]
       );
 
-      $this->add_control(
-         'view-all-url',
-         [
-            'label' => __( 'View all url', 'felipa' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => '#',
-            'condition' => [
-               'style' => 'two',
-            ]
-         ]
-      );
-
       $this->end_controls_section();
 
    }
@@ -125,11 +63,8 @@ class felipa_Widget_Portfolio extends Widget_Base {
       
       //Inline Editing
       $this->add_inline_editing_attributes( 'ppp', 'basic' );
-      $this->add_inline_editing_attributes( 'sub-title', 'basic' );
-      $this->add_inline_editing_attributes( 'title', 'basic' );
-      $this->add_inline_editing_attributes( 'deacription', 'basic' );
       ?>
-      <?php if ($settings['style']  == 'one') { ?>
+
       <div class="container-fluid" style="padding: 0; overflow: hidden;">
          <div class="portfolio">
             <?php
@@ -151,51 +86,6 @@ class felipa_Widget_Portfolio extends Widget_Base {
             <?php endwhile; wp_reset_postdata(); ?> 
          </div>
       </div>
-
-      <?php } elseif ($settings['style']  == 'two') {?>
-
-         
-      <div class="portfolio-container">
-         <?php if ('two' == $settings['style']  ): ?>
-         <div class="row swiper-title">
-            <div class="col-md-3">
-               <div class="section-title color" style="text-align: left">
-                 <span><?php echo esc_html( $settings['sub-title'] ); ?></span>
-                 <h1><?php echo esc_html( $settings['title'] ); ?></h1>
-              </div>
-            </div>
-            <div class="col-md-4">
-               <div class="swiper-pagination"></div>
-            </div>
-            <div class="col-md-2">
-               <div class="felipa-btn">
-                  <a href="<?php echo esc_url( $settings['view-all-url'] ); ?>">View gallery</a>
-               </div>
-            </div>
-         </div>
-         <?php endif; ?>
-         <div class="portfolio-2 swiper-wrapper">
-            <?php
-            $portfolio = new \WP_Query( array(
-            'post_type' => 'portfolio',
-            'posts_per_page' => $settings['ppp']['size']
-            ));
-            /* Start the Loop */
-            while ( $portfolio->have_posts() ) : $portfolio->the_post();
-            $portfolio_terms = get_the_terms( get_the_ID() , 'portfolio_category' );
-            ?>
-
-            <div class="portfolio-item-2 swiper-slide">
-               <a href="<?php the_permalink(); ?>">
-                  <?php the_post_thumbnail('felipa-470-375'); ?>
-                  <h5><?php echo wp_trim_words( get_the_title(), 3, '...' );?></h5>
-               </a>
-            </div>
-
-            <?php endwhile; wp_reset_postdata(); ?> 
-         </div>
-      </div>
-     <?php } ?>
 
    
       <?php }
--- inc/elementor/widgets/widget-portfolio3.php
+++ inc/elementor/widgets/widget-portfolio3.php
@@ -4,14 +4,14 @@ namespace Elementor;
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // portfolio
-class felipa_Widget_Portfolio2 extends Widget_Base {
+class felipa_Widget_portfolio3 extends Widget_Base {
  
    public function get_name() {
-      return 'portfolio2';
+      return 'portfolio3';
    }
  
    public function get_title() {
-      return esc_html__( 'Portfolio 2', 'felipa' );
+      return esc_html__( 'Portfolio 3', 'felipa' );
    }
  
    public function get_icon() { 
@@ -27,7 +27,7 @@ class felipa_Widget_Portfolio2 extends Widget_Base {
       $this->start_controls_section(
          'portfolio_section',
          [
-            'label' => esc_html__( 'Portfolio 2', 'felipa' ),
+            'label' => esc_html__( 'Portfolio 3', 'felipa' ),
             'type' => Controls_Manager::SECTION,
          ]
       );
@@ -104,46 +104,44 @@ class felipa_Widget_Portfolio2 extends Widget_Base {
       $this->add_inline_editing_attributes( 'url', 'basic' );
       ?>
 
-      <div class="portfolio-container">
-         <div class="row swiper-title">
-            <div class="col-md-3">
-               <div class="section-title color" style="text-align: left">
-                 <span><?php echo esc_html( $settings['sub-title'] ); ?></span>
-                 <h1><?php echo esc_html( $settings['title'] ); ?></h1>
-              </div>
-            </div>
-            <div class="col-md-4">
-               <div class="swiper-pagination"></div>
-            </div>
-            <div class="col-md-2">
-               <div class="felipa-btn">
-                  <a href="<?php echo esc_url( $settings['view-all-url'] ); ?>">View gallery</a>
+      <div class="container-fluid">
+         <div class="portfolio-left">
+            <div class="row">
+               <div class="col-md-3">
+                  <div class="section-title">
+                     <span><?php echo esc_html( $settings['sub-title'] ); ?></span>
+                     <h1><?php echo esc_html( $settings['title'] ); ?></h1>
+                     <p><?php echo esc_html( $settings['deacription'] ); ?></p>
+                     <div class="felipa-btn">
+                        <a href="<?php echo esc_url( $settings['url'] ); ?>">View gallery</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-9">
+                  <div class="portfolio-3">
+                     <?php
+                     $portfolio = new \WP_Query( array(
+                     'post_type' => 'portfolio',
+                     'posts_per_page' => $settings['ppp']['size']
+                     ));
+
+                     while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
+
+                     <div class="portfolio-item-3">
+                        <a href="<?php the_permalink(); ?>">
+                           <?php the_post_thumbnail('felipa-360-500'); ?>
+                           <h5><?php echo wp_trim_words( get_the_title(), 3, '...' );?></h5>
+                        </a>
+                     </div>
+
+                     <?php endwhile; wp_reset_postdata(); ?>
+                  </div>
                </div>
             </div>
-         </div>
-         <div class="portfolio-2 swiper-wrapper">
-            <?php
-            $portfolio = new \WP_Query( array(
-            'post_type' => 'portfolio',
-            'posts_per_page' => $settings['ppp']['size']
-            ));
-            /* Start the Loop */
-            while ( $portfolio->have_posts() ) : $portfolio->the_post();
-            $portfolio_terms = get_the_terms( get_the_ID() , 'portfolio_category' );
-            ?>
-
-            <div class="portfolio-item-2 swiper-slide">
-               <a href="<?php the_permalink(); ?>">
-                  <?php the_post_thumbnail('felipa-470-375'); ?>
-                  <h5><?php echo wp_trim_words( get_the_title(), 3, '...' );?></h5>
-               </a>
-            </div>
-
-            <?php endwhile; wp_reset_postdata(); ?> 
          </div>
       </div>
       <?php }
  
 }
 
-Plugin::instance()->widgets_manager->register_widget_type( new felipa_Widget_Portfolio2 );
+Plugin::instance()->widgets_manager->register_widget_type( new felipa_Widget_portfolio3 );
