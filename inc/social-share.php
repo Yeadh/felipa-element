b7fa6130553a0d9e165a7bb14f8fb24c51792345
Farhan Khan <Farhan01617@gmail.com>
2019-04-19 18:31:35 +0600

    update

--- inc/social-share.php
+++ inc/social-share.php
@@ -4,8 +4,8 @@ function felipaSocialShare() {?>
 
 	<div class="social-share">
         <ul class="list-inline">
-            <li class="list-inline-item"><a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=<?php the_permalink() ?>"><i class="fa fa-facebook"></i>Facebook</a></li>
-            <li class="list-inline-item"><a href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>+<?php the_permalink() ?>"><i class="fa fa-twitter"></i>Twitter</a></li>
+            <li class="list-inline-item"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>"><i class="fa fa-facebook"></i>Facebook</a></li>
+            <li class="list-inline-item"><a href="https://twitter.com/home?status=<?php the_permalink() ?>"><i class="fa fa-twitter"></i>Twitter</a></li>
             <li class="list-inline-item"><a href="https://plus.google.com/share?url=<?php the_permalink() ?>"><i class="fa fa-google-plus"> Google</i></a></li>
             <li class="list-inline-item"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>"><i class="fa fa-linkedin"></i>LinkedIn</a></li>
         </ul>
